8bf289a6e787bc8524115a394fdcc52a5c4d253f
Pravinthaa <pravinthaasree95173r@gmail.com>
2025-11-04 01:53:22 +0530

    added database

--- api.py
+++ api.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name = {self.name}"
+
+    
+
 @app.route('/')
 def home():
-    return '<h1>Welcome</h1>'
+    return '<h1>Flask REST API</h1>'
+
+@app.route('/add')
+def add_user():
+    mock_user = UserModel(name="Prav")
+    db.session.add(mock_user)
+    db.session.commit()
+    return jsonify({"message": "successssssssssss"})
 
 if __name__ == '__main__':
     app.run(debug=True)
