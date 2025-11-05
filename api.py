23fede6f4a4db240614d824d5931217c81c499fd
Pravinthaa <pravinthaasree95173r@gmail.com>
2025-11-05 23:45:09 +0530

    updated files

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
+api = Api(app)
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,64 @@ class UserModel(db.Model):
         return f"User(name = {self.name}"
 
     
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help="Name cannot be blank")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String
+}
+
+
+class Users(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+    @marshal_with(resource_fields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users,201
+    
+class User(Resource):
+    @marshal_with(resource_fields)
+    def get(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"User not found")
+        return user
+    
+    @marshal_with(resource_fields)
+    def patch(self,id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"User not found")
+        user.name = args["name"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(resource_fields)
+    def delete(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"User not found")
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users
+    
+ 
+    
+
+api.add_resource(Users,'/api/users/')
+api.add_resource(User,'/api/users/<int:id>')
+
 
 @app.route('/')
 def home():
